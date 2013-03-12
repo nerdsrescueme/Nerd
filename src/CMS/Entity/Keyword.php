4918bf3eb6fbdb61a9489b706c9ec069db544c06
Frank Bardon Jr <frankbardon@gmail.com>
2013-03-12 05:51:32 -0400

    Keyword entity now properly commented in code

--- src/CMS/Entity/Keyword.php
+++ src/CMS/Entity/Keyword.php
@@ -8,8 +8,14 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * Keyword entity
+ * 
  * @ORM\Entity
  * @ORM\Table(name="nerd_keywords")
+ * 
+ * @package NerdCMS
+ * @category Entities
+ * @author Frank Bardon Jr. <frank@nerdsrescue.me>
  */
 class Keyword
 {
@@ -22,6 +28,8 @@ class Keyword
 
     /**
      * @ORM\Column(type="string", length=32, nullable=false, unique=true)
+     * @Assert\NotBlank
+     * @Assert\Length(min=2, max=32)
      */
     protected $keyword;
 
@@ -30,17 +38,62 @@ class Keyword
      */
     protected $pages;
 
-
+    /**
+     * Constructor
+     * 
+     * @return this
+     */
     public function __construct()
     {
         $this->pages = new ArrayCollection();
     }
 
+    /**
+     * Return keyword id
+     * 
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return keyword
+     * 
+     * @return string
+     */
     public function getKeyword()
     {
         return $this->keyword;
     }
 
+    /**
+     * Get associated page entities
+     * 
+     * @return array[Page]
+     */
+    public function getPages()
+    {
+        return $this->pages;
+    }
+
+    /**
+     * Set keyword
+     * 
+     * @param string $keyword Keyword
+     * @return void
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = trim($keyword);
+    }
+
+    /**
+     * String object representation
+     * 
+     * @return string
+     */
     public function __toString()
     {
         return $this->getKeyword();
